53a2916badff73efe0414e3d2fee2703a2617ece
Syps <nsypteras@gmail.com>
2017-06-10 18:35:39 -0400

    update footer

--- components/storyfooter.js
+++ components/storyfooter.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import FontAwesome from 'react-fontawesome'
 
 
 export default ({ githubLink }) => {
+  const home = "/"
   return (
     <div className="row">
       <div id="footer">
-          <span className="footer-item">
-            [<a target="_blank" href={ githubLink }>code</a>]
-          </span>
+        <div className='footer-top'>
+          <a target="_blank" href={ githubLink }>
+            <FontAwesome className='social-media-icon' name='github-alt' />
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/nicholas-sypteras-0a24a969">
+            <FontAwesome className='social-media-icon' name='linkedin' />
+          </a>
+        </div>
+        <div className='footer-bottom'>
+          <a className='home-link'
+            href={ home }>nick sypteras</a>
+        </div>
       </div>
     </div>
   )
